f25fe7f223217f4afcd47cabc666c27bd50dfeda
ericjohnsonclaim <ericjohnsonclaim@gmail.com>
2026-04-05 15:03:26 -0400

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,22 @@
+export default function handler(req, res) {
+  const clientId = process.env.SPOTIFY_CLIENT_ID
+  const redirectUri = process.env.REDIRECT_URI
+
+  const scopes = [
+    'streaming',
+    'user-read-email',
+    'user-read-private',
+    'user-read-playback-state',
+    'user-modify-playback-state'
+  ].join(' ')
+
+  const params = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scopes,
+    redirect_uri: redirectUri,
+    show_dialog: 'false'
+  })
+
+  res.redirect(`https://accounts.spotify.com/authorize?${params}`)
+}
